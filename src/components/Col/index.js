5581abd097db965e04e437e3122b162c7efe7a49
Tyler Kennedy <tkennedy118@gmail.com>
2020-04-23 22:04:56 -0500

    add row, col, and container components

---
+++ src/components/Col/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Col(props) {
+  const size = props.size.split(" ").map(size => "col-" + size).join(" ");
+
+  return <div className={size} {...props} />;
+}
