8f230af208f6fb00d51f05fee64dcd54992f3ff7
Tyler Kennedy <tkennedy118@gmail.com>
2020-04-23 21:50:18 -0500

    implement simple navbar and create route connections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
+import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Nav from './components/Nav/index';
 
@@ -10,7 +10,7 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path={['/', '/home']} component={Home} />
+          <Route exact path={['/', '/about']} component={About} />
           <Route exact path={'/portfolio'} component={Portfolio} />
         </Switch>
       </div>
