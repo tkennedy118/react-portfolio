f50badd6e9ee36dec3466296fed61ff251ed476e
Tyler Kennedy <tkennedy118@gmail.com>
2020-04-26 20:15:29 -0500

    create fade-in transition for portfolio cards

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import { Navbar as BootsNavbar } from 'react-bootstrap';
 export default function Navbar() {
 
   return(
-    <BootsNavbar  variant='light' bg='light'>
+    <BootsNavbar  variant='light' bg='light' className='text-center'>
       <BootsNavbar.Brand href='/' className='nav-font-small'>TK</BootsNavbar.Brand>
     </BootsNavbar>
   )
--- src/pages/About.js
+++ src/pages/About.js
@@ -39,21 +39,21 @@ export default function About() {
       </Jumbotron>
       <Jumbotron className='py-0 my-0 about-jumbotron-red' fluid>
         <Container>
-            <Row>
-              <Col md={7}>
-                <h3 className='about-text-blue about-font-medium text-center my-4'>Specs.</h3>
-                <p className='about-text-white text-center'>
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                  when an unknown printer took a galley of type and scrambled it to make a type 
-                  specimen book. It has survived not only five centuries, but also the leap into 
-                  electronic typesetting, remaining essentially unchanged. 
-                </p>
-              </Col>
-              <Col md={5} className='my-auto'>
-                <Image src={require('../utils/images/paint-blue.png')} alt='Blue Paint' fluid />
-              </Col>
-            </Row>
+          <Row>
+            <Col md={7}>
+              <h3 className='about-text-blue about-font-medium text-center my-4'>Specs.</h3>
+              <p className='about-text-white text-center'>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                when an unknown printer took a galley of type and scrambled it to make a type 
+                specimen book. It has survived not only five centuries, but also the leap into 
+                electronic typesetting, remaining essentially unchanged. 
+              </p>
+            </Col>
+            <Col md={5} className='my-auto'>
+              <Image src={require('../utils/images/paint-blue.png')} alt='Blue Paint' fluid />
+            </Col>
+          </Row>
         </Container>
       </Jumbotron>
     </>
