6ac0cf5ef8693b4740c34825e22579626a139fab
Tyler Kennedy <tkennedy118@gmail.com>
2020-04-28 09:04:54 -0500

    fix profile pic resizing

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,53 +5,45 @@ import '../utils/styles/about.css';
 export default function About() {
   return (
     <>
-      <Jumbotron className='py-0 my-0 about-jumbotron-gray' fluid>
+      <Jumbotron className='py-0 my-0 about-jumbotron-background' fluid>
         <Container>
           <Row>
             <Col md={7} className='text-center my-5 my-md-auto'>
-              <h2 className='about-text-red about-font-large'>Tyler Kennedy</h2>
-              <p className='about-text-red'>I am an observer, a learner, and an achiever.</p>
+              <h2>
+                <span className='vl-blue'></span>Tyler Kennedy
+              </h2>
+              <p>I am an observer, a learner, and an achiever.</p>
             </Col>
-            <Col md={5}>
-              <Image src={require('../utils/images/portfolio.png')} alt='Tyler Kennedy' fluid />
+            <Col md={5} className='text-center'>
+              <Image id='profile-pic' src={require('../utils/images/portfolio.png')} alt='Tyler Kennedy' fluid />
             </Col>
           </Row>
         </Container>
       </Jumbotron>
-      <Jumbotron className='py-0 my-0 about-jumbotron-blue' fluid>
+      <Jumbotron className='my-0 about-jumbotron-background' fluid>
         <Container>
           <Row>
-            <Col md={5} className='my-auto'>
-            <Image src={require('../utils/images/paint-red.png')} alt='Red Paint' fluid />
+            <Col md={{ span: 6, offset: 3 }}>
+              <h3 className='text-center my-4'>What you should know about me.</h3>
+              <p className='text-center'>
+                
+              </p>
             </Col>
-            <Col md={7}>
-              <h3 className='about-text-white about-font-medium text-center my-4'>About me.</h3>
-              <p className='about-text-white text-center'>
+          </Row>
+        </Container>
+      </Jumbotron>
+      <Jumbotron className='py-0 my-0 about-jumbotron-background' fluid>
+        <Container>
+          <Row>
+            <Col md={{ span: 7, offset: 1 }}>
+              <h3 className='text-center my-4'>What can I do?</h3>
+              <p className='text-center'>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
                 when an unknown printer took a galley of type and scrambled it to make a type 
                 specimen book. It has survived not only five centuries, but also the leap into 
                 electronic typesetting, remaining essentially unchanged. 
               </p>
-            </Col>
-          </Row>
-        </Container>
-      </Jumbotron>
-      <Jumbotron className='py-0 my-0 about-jumbotron-red' fluid>
-        <Container>
-          <Row>
-            <Col md={7}>
-              <h3 className='about-text-blue about-font-medium text-center my-4'>Specs.</h3>
-              <p className='about-text-white text-center'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                when an unknown printer took a galley of type and scrambled it to make a type 
-                specimen book. It has survived not only five centuries, but also the leap into 
-                electronic typesetting, remaining essentially unchanged. 
-              </p>
-            </Col>
-            <Col md={5} className='my-auto'>
-              <Image src={require('../utils/images/paint-blue.png')} alt='Blue Paint' fluid />
             </Col>
           </Row>
         </Container>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -24,6 +24,20 @@ const projects = [
     techs: ['Express', 'Handlebars', 'Passport', 'Sequelize'],
     image: 'covid-19.png',
     link: 'https://novel-19.herokuapp.com/home'
+  },
+  {
+    title: 'Novel 19',
+    description: 'A mobile-friendly website designed to assist users in visualizing coronavirus cases in their region.',
+    techs: ['Express', 'Handlebars', 'Passport', 'Sequelize'],
+    image: 'covid-19.png',
+    link: 'https://novel-19.herokuapp.com/home'
+  },
+  {
+    title: 'Novel 19',
+    description: 'A mobile-friendly website designed to assist users in visualizing coronavirus cases in their region.',
+    techs: ['Express', 'Handlebars', 'Passport', 'Sequelize'],
+    image: 'covid-19.png',
+    link: 'https://novel-19.herokuapp.com/home'
   }
 ]
 
@@ -31,7 +45,7 @@ export default function Portfolio() {
 
   const cards = projects.map((project, index) => {
     return (
-      <Card key={index} className='rounded-0 my-3 shadow-lg port-card'>
+      <Card key={index} className='rounded-0 my-3 shadow-sm port-card'>
         <Row>
           <Col md={6}>
             <Card.Img src={require('../utils/images/' + project.image)} className='rounded-0'/>
@@ -52,7 +66,7 @@ export default function Portfolio() {
   // const [cards, setCards] = useState([...cards])
   const transitions = useTransition(cards, card => card.key, {
     from: { opacity: 0, marginLeft: -100, marginRight: 100 },
-    enter: { opacity: 1, marginLeft: 0, marginRight: 0 }
+    enter: { opacity: 1, marginLeft: 0, marginRight: 0 },
   });
   
   return (
