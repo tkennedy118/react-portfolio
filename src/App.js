09639ce2895b11debad6e4621791fb57db22002e
Tyler Kennedy <tkennedy118@gmail.com>
2020-05-14 22:04:42 -0500

    update scrollbar sizes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './app.css';
 import About from './pages/About/index';
 import Navbar from './components/Navbar/index';
 import Footer from './components/Footer/index';
+import './app.css';
 
 function App() {
   
